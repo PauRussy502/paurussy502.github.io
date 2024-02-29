42dc47eff6430ecb8f292d81f841dc9b913eabe4
RUSSY <paurussy@gmail.com>
2024-02-29 12:41:23 +0100

    Mobile News Ban

--- js/mobiledetect.js
+++ js/mobiledetect.js
@@ -1,8 +1,14 @@
 window.onload = function() {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         var elements = document.getElementsByClassName('mobiledisplay');
+        var elementsalert = document.getElementsByClassName('mobilealert');
         for (var i = 0; i < elements.length; i++) {
             elements[i].style.display = 'block';
+        }
+        for (var i = 0; i < elementsalert.length; i++) {
+            elementsalert[i].addEventListener("click", onclickmb);
+            elementsalert[i].setAttribute('href', "#");
+            elementsalert[i].classList.add("mobilealerton");
         }
     }
     else {
@@ -12,3 +18,7 @@ window.onload = function() {
         }
     }
 };
+
+function onclickmb() {
+    alert("No avaliable in Mobile")
+}
